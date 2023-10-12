7fa5a9f7351d626e6f162c9f0a986c3d81a6e9eb
Maurício J <mrc.rsw.98@gmail.com>
2023-10-12 14:47:24 -0300

    Milagre 2º

--- front/src/app/components/template/carousel/carousel.component.ts
+++ front/src/app/components/template/carousel/carousel.component.ts
@@ -8,10 +8,29 @@ import { FlashcardsService } from 'src/app/services/flashcards.service';
   styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
-  localFlashcards: IFlashcard[] = [];
   focus1: boolean = true;
   focus2: boolean = false;
   focus3: boolean = false;
+
+  teste(arg1: number) {
+    if (arg1 == 1) {
+      this.focus1 = true;
+      this.focus2 = false;
+      this.focus3 = false;
+    }
+    if (arg1 == 2) {
+      this.focus1 = false;
+      this.focus2 = true;
+      this.focus3 = false;
+    }
+    if (arg1 == 3) {
+      this.focus1 = false;
+      this.focus2 = false;
+      this.focus3 = true;
+    }
+  }
+  localFlashcards: IFlashcard[] = [];
+  // focus: boolean = true;
   constructor(public FCService: FlashcardsService) {}
   ngOnInit(): void {
     this.FCService.flashcards.subscribe((data) => {
